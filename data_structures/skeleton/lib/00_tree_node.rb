a20a3d9a1a4cb9ad51515ad83d7f4aae1dc2fe3c
JordanYu4 <jordanyu44@gmail.com>
2018-06-22 11:31:28 -0700

    Add add_child and remove_child methods to TreeNode

--- data_structures/skeleton/lib/00_tree_node.rb
+++ data_structures/skeleton/lib/00_tree_node.rb
@@ -23,4 +23,19 @@ class PolyTreeNode
     @parent = node
     node.children << self unless node.nil? || node.children.include?(self)
   end
+  
+  def add_child(child_node)
+    @children << child_node unless @children.include?(child_node)
+    child_node.parent = self
+  end
+  
+  def remove_child(child_node)
+    if child_node.parent != self
+      raise "Not a child"
+    end
+    child_node.parent = nil
+    @children.delete(child_node)
+    
+  end
+  
 end
